fab1a1f84489b0a36790ff6cedcd9f3ece237c86
user <email>
2022-11-07 22:18:38 +0300

    Add index.html

--- generate/ui.js
+++ generate/ui.js
@@ -27,6 +27,25 @@ export default function App() {
 }`;
 	fs.writeFileSync(appName+"/ui/src/app.js",data);
 }
+const generateIndexHTML=(appName)=>
+{
+	const data=`
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <link rel="icon" href="%PUBLIC_URL%/favicon.ico" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="theme-color" content="#000000" />
+    <title>`+appName+`</title>
+  </head>
+  <body>
+    <noscript>You need to enable JavaScript to run this app.</noscript>
+    <div id="root"></div>
+  </body>
+</html>`;
+	fs.writeFileSync(appName+"/ui/public/index.html",data);
+}
 const appName=process.argv[2];
 fs.mkdirSync(appName);
 fs.mkdirSync(appName+"/ui/");
@@ -34,3 +53,4 @@ fs.mkdirSync(appName+"/ui/src");
 fs.mkdirSync(appName+"/ui/public");
 generateIndex(appName);
 generateApp(appName);
+generateIndexHTML(appName);
