c0df52b5e9b7afee15993c3cc6a5605d5200ecf8
tunc1 <email>
2022-12-13 21:39:15 +0300

    Add Dependency

--- client/src/App.js
+++ client/src/App.js
@@ -8,11 +8,37 @@ import Dependency from "./components/dependency/Dependency";
 import {useState} from "react"; 
 import Modal from "./components/modal/Modal";
 
-function App() {
+function App()
+{
 	const API_URL=process.env.REACT_APP_API;
 	const [isDarkModeOn,setIsDarkModeOn]=useState(false);
+	const [showModal, setShowModal]=useState(false);
 	const [projectDetails,setProjectDetails]=useState({name:"",description:"",version:"1.0"});
-	const [selectedDependencies,setSelectedDependencies]=useState([{name:"Axios",version:"1.2.0",description:"Axios desc",dev:false},{name:"Nodemon",version:"14.2.0",description:"Nodemon desc",dev:true}])
+	const [dependenciesToShow,setDependenciesToShow]=useState([{name:"Axios",version:"1.2.0",description:"Axios desc"},{name:"Nodemon",version:"14.2.0",description:"Nodemon desc"}])
+	const [selectedDependencies,setSelectedDependencies]=useState([]);
+	const [dependenciesToAdd,setDependenciesToAdd]=useState([]);
+	const onAddDependencyClick=()=>
+	{
+		setShowModal(true);
+	};
+	const onRemoveDependencyClick=dependency=>
+	{
+		delete dependency.dev;
+		console.log(dependency);
+		setSelectedDependencies(selectedDependencies.filter(each=>each.name!==dependency.name));
+		setDependenciesToShow([...dependenciesToShow,dependency]);
+	}
+	const onDependencySelected=dependency=>
+	{
+		setDependenciesToAdd([...dependenciesToAdd,{...dependency,dev:false}]);
+		setDependenciesToShow(dependenciesToShow.filter(each=>each.name!=dependency.name));
+	};
+	const addDependencySaveOnClick=()=>
+	{
+		setShowModal(false);
+		setSelectedDependencies([...selectedDependencies,...dependenciesToAdd]);
+		setDependenciesToAdd([]);
+	};
 	const onDownloadClick=()=>
 	{
 		const urlSearchParams=new URLSearchParams(projectDetails).toString();
@@ -22,7 +48,7 @@ function App() {
 		document.body.appendChild(anchor);
 		anchor.click();
 		document.body.removeChild(anchor);
-	}
+	};
 	return (
 		<div className="flex">
 			<div className="sidebar-left">
@@ -38,8 +64,8 @@ function App() {
 						<ProjectInput projectDetails={projectDetails} setProjectDetails={setProjectDetails}/> 
 					</div>
 					<div className="right overflow-auto">
-						<Modal/>
-						<Dependency selectedDependencies={selectedDependencies} setSelectedDependencies={setSelectedDependencies}/>
+						<Modal showModal={showModal} setShowModal={setShowModal} dependenciesToShow={dependenciesToShow} onDependencySelected={onDependencySelected} addDependencySaveOnClick={addDependencySaveOnClick}/>
+						<Dependency onRemoveDependencyClick ={onRemoveDependencyClick } onAddDependencyClick={onAddDependencyClick} selectedDependencies={selectedDependencies} setSelectedDependencies={setSelectedDependencies}/>
 					</div>
 				</div>
 				<Action onDownloadClick={onDownloadClick}/>
--- client/src/components/modal/Modal.js
+++ client/src/components/modal/Modal.js
@@ -1,286 +1,69 @@
-import React from 'react';
 import { AiOutlineSearch } from 'react-icons/ai';
-export default function Modal() {
-  const [showModal, setShowModal] = React.useState(false);
-  return (
-    <>
-      <button
-        className='bg-pink-500 text-white active:bg-pink-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
-        type='button'
-        onClick={() => setShowModal(true)}
-      >
-        Open Modal
-      </button>
-      {showModal ? (
-        <>
-          <div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'>
-            <div className='relative w-auto my-6 mx-auto max-w-3xl'>
-              {/*content*/}
-              <div className='border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
-                {/*header*/}
-                <div className='flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t w-full'>
-                  <div className='flex items-center px-2 sticky top-0 bg-white w-full '>
-                    <AiOutlineSearch size={24} className='text-gray-700 w-1/6' />
-                    <input
-                      type='text'
-                      placeholder='Enter Dependency name'
-                      className='placeholder:text-gray-200 p-2 outline-none border w-full '
-                    />
-                  </div>
-                  <button
-                    className='p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none'
-                    onClick={() => setShowModal(false)}
-                  >
-                    <span className='bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none'>
-                      ×
-                    </span>
-                  </button>
-                </div>
-                {/*body*/}
-                <div className='relative p-6 flex-auto overflow-y-auto md:h-96'>
-                <div class="bg-gray-200 flex flex-col   items-center justify-center ">
-  
-  <button class="border border-black rounded-sm w-full grid grid-cols-12 bg-white shadow p-3 gap-2 items-center hover:shadow-lg transition delay-150 duration-300 ease-in-out hover:scale-105 transform" >
-    
-    {/* <div class="col-span-12 md:col-span-1">
-      
-    </div> */}
-    
-    <div class="col-span-11 xl:-ml-5">
-      <p class="text-blue-600 font-semibold"> Axios<span className="ml-2 text-red-500">1.0.2</span></p>
-    </div>
-    
-    <div class="md:col-start-2 col-span-11 xl:-ml-5">
-      <p class="text-sm text-gray-800 font-light"> Axios http modülünü daha kolay bir hale getirip senin daha rahat istekler atmanı sağlar. </p>
-    </div>
-    
-  </button>
-  <button class="border border-black rounded-sm w-full grid grid-cols-12 bg-white shadow p-3 gap-2 items-center hover:shadow-lg transition delay-150 duration-300 ease-in-out hover:scale-105 transform" >
-    
-    {/* <div class="col-span-12 md:col-span-1">
-      
-    </div> */}
-    
-    <div class="col-span-11 xl:-ml-5">
-      <p class="text-blue-600 font-semibold"> Axios<span className="ml-2 text-red-500">1.0.2</span></p>
-    </div>
-    
-    <div class="md:col-start-2 col-span-11 xl:-ml-5">
-      <p class="text-sm text-gray-800 font-light"> Axios http modülünü daha kolay bir hale getirip senin daha rahat istekler atmanı sağlar. </p>
-    </div>
-    
-  </button>
-  <button class="border border-black rounded-sm w-full grid grid-cols-12 bg-white shadow p-3 gap-2 items-center hover:shadow-lg transition delay-150 duration-300 ease-in-out hover:scale-105 transform" >
-    
-    {/* <div class="col-span-12 md:col-span-1">
-      
-    </div> */}
-    
-    <div class="col-span-11 xl:-ml-5">
-      <p class="text-blue-600 font-semibold"> Axios<span className="ml-2 text-red-500">1.0.2</span></p>
-    </div>
-    
-    <div class="md:col-start-2 col-span-11 xl:-ml-5">
-      <p class="text-sm text-gray-800 font-light"> Axios http modülünü daha kolay bir hale getirip senin daha rahat istekler atmanı sağlar. </p>
-    </div>
-    
-  </button>
-  <button class="border border-black rounded-sm w-full grid grid-cols-12 bg-white shadow p-3 gap-2 items-center hover:shadow-lg transition delay-150 duration-300 ease-in-out hover:scale-105 transform" >
-    
-    {/* <div class="col-span-12 md:col-span-1">
-      
-    </div> */}
-    
-    <div class="col-span-11 xl:-ml-5">
-      <p class="text-blue-600 font-semibold"> Axios<span className="ml-2 text-red-500">1.0.2</span></p>
-    </div>
-    
-    <div class="md:col-start-2 col-span-11 xl:-ml-5">
-      <p class="text-sm text-gray-800 font-light"> Axios http modülünü daha kolay bir hale getirip senin daha rahat istekler atmanı sağlar. </p>
-    </div>
-    
-  </button>
-  <button class="border border-black rounded-sm w-full grid grid-cols-12 bg-white shadow p-3 gap-2 items-center hover:shadow-lg transition delay-150 duration-300 ease-in-out hover:scale-105 transform" >
-    
-    {/* <div class="col-span-12 md:col-span-1">
-      
-    </div> */}
-    
-    <div class="col-span-11 xl:-ml-5">
-      <p class="text-blue-600 font-semibold"> Axios<span className="ml-2 text-red-500">1.0.2</span></p>
-    </div>
-    
-    <div class="md:col-start-2 col-span-11 xl:-ml-5">
-      <p class="text-sm text-gray-800 font-light"> Axios http modülünü daha kolay bir hale getirip senin daha rahat istekler atmanı sağlar. </p>
-    </div>
-    
-  </button>
-  <button class="border border-black rounded-sm w-full grid grid-cols-12 bg-white shadow p-3 gap-2 items-center hover:shadow-lg transition delay-150 duration-300 ease-in-out hover:scale-105 transform" >
-    
-    {/* <div class="col-span-12 md:col-span-1">
-      
-    </div> */}
-    
-    <div class="col-span-11 xl:-ml-5">
-      <p class="text-blue-600 font-semibold"> Axios<span className="ml-2 text-red-500">1.0.2</span></p>
-    </div>
-    
-    <div class="md:col-start-2 col-span-11 xl:-ml-5">
-      <p class="text-sm text-gray-800 font-light"> Axios http modülünü daha kolay bir hale getirip senin daha rahat istekler atmanı sağlar. </p>
-    </div>
-    
-  </button>
-  <button class="border border-black rounded-sm w-full grid grid-cols-12 bg-white shadow p-3 gap-2 items-center hover:shadow-lg transition delay-150 duration-300 ease-in-out hover:scale-105 transform" >
-    
-    {/* <div class="col-span-12 md:col-span-1">
-      
-    </div> */}
-    
-    <div class="col-span-11 xl:-ml-5">
-      <p class="text-blue-600 font-semibold"> Axios<span className="ml-2 text-red-500">1.0.2</span></p>
-    </div>
-    
-    <div class="md:col-start-2 col-span-11 xl:-ml-5">
-      <p class="text-sm text-gray-800 font-light"> Axios http modülünü daha kolay bir hale getirip senin daha rahat istekler atmanı sağlar. </p>
-    </div>
-    
-  </button>
-  <button class="border border-black rounded-sm w-full grid grid-cols-12 bg-white shadow p-3 gap-2 items-center hover:shadow-lg transition delay-150 duration-300 ease-in-out hover:scale-105 transform" >
-    
-    {/* <div class="col-span-12 md:col-span-1">
-      
-    </div> */}
-    
-    <div class="col-span-11 xl:-ml-5">
-      <p class="text-blue-600 font-semibold"> Axios<span className="ml-2 text-red-500">1.0.2</span></p>
-    </div>
-    
-    <div class="md:col-start-2 col-span-11 xl:-ml-5">
-      <p class="text-sm text-gray-800 font-light"> Axios http modülünü daha kolay bir hale getirip senin daha rahat istekler atmanı sağlar. </p>
-    </div>
-    
-  </button>
-  <button class="border border-black rounded-sm w-full grid grid-cols-12 bg-white shadow p-3 gap-2 items-center hover:shadow-lg transition delay-150 duration-300 ease-in-out hover:scale-105 transform" >
-    
-    {/* <div class="col-span-12 md:col-span-1">
-      
-    </div> */}
-    
-    <div class="col-span-11 xl:-ml-5">
-      <p class="text-blue-600 font-semibold"> Axios<span className="ml-2 text-red-500">1.0.2</span></p>
-    </div>
-    
-    <div class="md:col-start-2 col-span-11 xl:-ml-5">
-      <p class="text-sm text-gray-800 font-light"> Axios http modülünü daha kolay bir hale getirip senin daha rahat istekler atmanı sağlar. </p>
-    </div>
-    
-  </button>
-  <button class="border border-black rounded-sm w-full grid grid-cols-12 bg-white shadow p-3 gap-2 items-center hover:shadow-lg transition delay-150 duration-300 ease-in-out hover:scale-105 transform" >
-    
-    {/* <div class="col-span-12 md:col-span-1">
-      
-    </div> */}
-    
-    <div class="col-span-11 xl:-ml-5">
-      <p class="text-blue-600 font-semibold"> Axios<span className="ml-2 text-red-500">1.0.2</span></p>
-    </div>
-    
-    <div class="md:col-start-2 col-span-11 xl:-ml-5">
-      <p class="text-sm text-gray-800 font-light"> Axios http modülünü daha kolay bir hale getirip senin daha rahat istekler atmanı sağlar. </p>
-    </div>
-    
-  </button>
-  <button class="border border-black rounded-sm w-full grid grid-cols-12 bg-white shadow p-3 gap-2 items-center hover:shadow-lg transition delay-150 duration-300 ease-in-out hover:scale-105 transform" >
-    
-    {/* <div class="col-span-12 md:col-span-1">
-      
-    </div> */}
-    
-    <div class="col-span-11 xl:-ml-5">
-      <p class="text-blue-600 font-semibold"> Axios<span className="ml-2 text-red-500">1.0.2</span></p>
-    </div>
-    
-    <div class="md:col-start-2 col-span-11 xl:-ml-5">
-      <p class="text-sm text-gray-800 font-light"> Axios http modülünü daha kolay bir hale getirip senin daha rahat istekler atmanı sağlar. </p>
-    </div>
-    
-  </button>
-  <button class="border border-black rounded-sm w-full grid grid-cols-12 bg-white shadow p-3 gap-2 items-center hover:shadow-lg transition delay-150 duration-300 ease-in-out hover:scale-105 transform" >
-    
-    {/* <div class="col-span-12 md:col-span-1">
-      
-    </div> */}
-    
-    <div class="col-span-11 xl:-ml-5">
-      <p class="text-blue-600 font-semibold"> Axios<span className="ml-2 text-red-500">1.0.2</span></p>
-    </div>
-    
-    <div class="md:col-start-2 col-span-11 xl:-ml-5">
-      <p class="text-sm text-gray-800 font-light"> Axios http modülünü daha kolay bir hale getirip senin daha rahat istekler atmanı sağlar. </p>
-    </div>
-    
-  </button>
-  <button class="border border-black rounded-sm w-full grid grid-cols-12 bg-white shadow p-3 gap-2 items-center hover:shadow-lg transition delay-150 duration-300 ease-in-out hover:scale-105 transform" >
-    
-    {/* <div class="col-span-12 md:col-span-1">
-      
-    </div> */}
-    
-    <div class="col-span-11 xl:-ml-5">
-      <p class="text-blue-600 font-semibold"> Axios<span className="ml-2 text-red-500">1.0.2</span></p>
-    </div>
-    
-    <div class="md:col-start-2 col-span-11 xl:-ml-5">
-      <p class="text-sm text-gray-800 font-light"> Axios http modülünü daha kolay bir hale getirip senin daha rahat istekler atmanı sağlar. </p>
-    </div>
-    
-  </button>
-  <button class="border border-black rounded-sm w-full grid grid-cols-12 bg-white shadow p-3 gap-2 items-center hover:shadow-lg transition delay-150 duration-300 ease-in-out hover:scale-105 transform" >
-    
-    {/* <div class="col-span-12 md:col-span-1">
-      
-    </div> */}
-    
-    <div class="col-span-11 xl:-ml-5">
-      <p class="text-blue-600 font-semibold"> Axios<span className="ml-2 text-red-500">1.0.2</span></p>
-    </div>
-    
-    <div class="md:col-start-2 col-span-11 xl:-ml-5">
-      <p class="text-sm text-gray-800 font-light"> Axios http modülünü daha kolay bir hale getirip senin daha rahat istekler atmanı sağlar. </p>
-    </div>
-    
-  </button>
-  
-  
-  
-  
-  
-  
-</div>
-                </div>
-                {/*footer*/}
-                <div className='flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b'>
-                  <button
-                    className='text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
-                    type='button'
-                    onClick={() => setShowModal(false)}
-                  >
-                    Close
-                  </button>
-                  <button
-                    className='bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
-                    type='button'
-                    onClick={() => setShowModal(false)}
-                  >
-                    Save
-                  </button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className='opacity-25 fixed inset-0 z-40 bg-black'></div>
-        </>
-      ) : null}
-    </>
-  );
+
+export default function Modal({showModal, setShowModal, dependenciesToShow, onDependencySelected,addDependencySaveOnClick}) {
+	return (
+	<>
+		{showModal ? (
+			<>
+				<div className='justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none'>
+					<div className='relative w-auto my-6 mx-auto max-w-3xl'>
+						<div className='border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none'>
+							<div className='flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t w-full'>
+								<div className='flex items-center px-2 sticky top-0 bg-white w-full '>
+									<AiOutlineSearch size={24} className='text-gray-700 w-1/6' />
+									<input
+									type='text'
+									placeholder='Enter Dependency name'
+									className='placeholder:text-gray-200 p-2 outline-none border w-full '
+									/>
+								</div>
+								<button
+								className='p-1 ml-auto bg-transparent border-0 text-black opacity-5 float-right text-3xl leading-none font-semibold outline-none focus:outline-none'
+								onClick={() => setShowModal(false)}
+								>
+									<span className='bg-transparent text-black opacity-5 h-6 w-6 text-2xl block outline-none focus:outline-none'>
+									×
+									</span>
+								</button>
+							</div>
+							<div className='relative p-6 flex-auto overflow-y-auto md:h-96'>
+								<div className="bg-gray-200 flex flex-col   items-center justify-center ">
+									{
+										dependenciesToShow.map(each=>
+										<button key={each.name} onClick={()=>onDependencySelected(each)} className="border border-black rounded-sm w-full grid grid-cols-12 bg-white shadow p-3 gap-2 items-center hover:shadow-lg transition delay-150 duration-300 ease-in-out hover:scale-105 transform" >
+											<div className="col-span-11 xl:-ml-5">
+												<p className="text-blue-600 font-semibold"> {each.name}<span className="ml-2 text-red-500">{each.version}</span></p>
+											</div>
+											<div className="md:col-start-2 col-span-11 xl:-ml-5">
+												<p className="text-sm text-gray-800 font-light"> {each.description} </p>
+											</div>
+										</button>)
+									}
+									
+								</div>
+							</div>
+							<div className='flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b'>
+								<button
+								className='text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
+								type='button'
+								onClick={() => setShowModal(false)}
+								>
+								Close
+								</button>
+								<button
+								className='bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150'
+								type='button'
+								onClick={addDependencySaveOnClick}
+								>
+								Save
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className='opacity-25 fixed inset-0 z-40 bg-black'></div>
+			</>
+		) : null}
+	</>
+	);
 }
